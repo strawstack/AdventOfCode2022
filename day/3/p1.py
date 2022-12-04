080f2fbdcebadd69f74fce53b50044cf71b3e165
strawstack <rhshadowgreen@gmail.com>
2022-12-04 10:47:17 -0800

    clean up day 3 solution

--- day/3/p1.py
+++ day/3/p1.py
@@ -1,26 +1,26 @@
-def same(done, dtwo):
-    for k in done:
-        if k in dtwo:
-            return k
-    raise "not found"
-
 def pri(letter):
     if ord(letter) > ord("Z"):
-        return ord(letter) - ord("A") + 27
-    else:
         return ord(letter) - ord("a") + 1
+    else:
+        return ord(letter) - ord("A") + 27
 
 def sol(lines):
-    total = 0
-    for line in lines:
-        length = len(line)//2
-        one, two = line[:length], line[length:]
-        done = {x: True for x in one}
-        dtwo = {x: True for x in two}
-        letter = same(done, dtwo)
-        p = pri(letter)
-        total += p
-    return total
+
+    return sum(
+        [
+            pri(
+                set(
+                    line[:len(line)//2]
+                )
+                .intersection(
+                    set(
+                        line[len(line)//2:]
+                    )
+                ).pop()
+            )
+            for line in lines
+        ]
+    )
 
 def main():
     
--- day/3/p2.py
+++ day/3/p2.py
@@ -1,44 +1,34 @@
-def same(done, dtwo):
-    for k in done:
-        if k in dtwo:
-            return k
-    raise "not found"
-
 def pri(letter):
     if ord(letter) > ord("Z"):
         return ord(letter) - ord("a") + 1
     else:
         return ord(letter) - ord("A") + 27
 
-def find3(group):
-    for x in group[0]:
-        if x in group[1] and x in group[2]:
-            return x
-    raise "not found"
-
 def sol(lines):
 
-    total = 0
-    for i in range(0, len(lines), 3):
-
-        group = [
-            lines[i],
-            lines[i + 1],
-            lines[i + 2]
+    return sum(
+        [
+            pri(
+                set(
+                    lines.pop()
+                )
+                .intersection(
+                    set(
+                        lines.pop()
+                    )
+                ).intersection(
+                    set(
+                        lines.pop()
+                    )
+                ).pop()
+            )
+            for x in range(len(lines)//3)
         ]
-
-        d = []
-        for bag in group:
-            d.append({x: True for x in bag})
-
-        letter = find3(d)
-        p = pri(letter)
-        total += p
-
-    return total
+    )
 
 def main():
     
+    #lines = open("input.txt").read().strip()
     lines = [x[:-1] for x in open("input.txt").readlines()]
     
     ans = sol(lines)
@@ -48,6 +38,3 @@ def main():
 # cp p1.py p2.py
 ans = main()
 print(ans)
-
-# 2863 no
-# 3037 no
